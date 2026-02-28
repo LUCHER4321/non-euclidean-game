ea8ff7bcba1757fa39966e97345d214233dc3ad0
LUCHER4321 <lucher4321@gmail.com>
2026-02-27 23:57:25 -0300

    Update Portal.cs

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 
 public class Portal : MonoBehaviour
 {
-    [SerializeField]
-    Camera inputCam;
     [SerializeField]
     Portal linkedPortal;
     [SerializeField]
@@ -12,12 +11,11 @@ public class Portal : MonoBehaviour
     [SerializeField]
     bool teleport = true;
     private RenderTexture rt;
-    private Dictionary<Collider, int> teleportObjects;
+    private Dictionary<Collider, GameObject> copies;
 
     void Awake()
     {
-        teleportObjects = new Dictionary<Collider, int>();
-        inputCam = Camera.main;
+        copies = new Dictionary<Collider, GameObject>();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -40,8 +38,8 @@ public class Portal : MonoBehaviour
         cam.targetTexture = rt;
         Material newMaterial = new Material(PortalST.Instance.GetGraph);
         newMaterial.SetTexture(PortalST.Instance.GetInputName, rt);
-        Renderer rndr = linkedPortal.GetComponent<Renderer>();
-        if (rndr != null) rndr.material = newMaterial;
+        Renderer rndr = linkedPortal != null ? linkedPortal.GetComponent<Renderer>() : null;
+        if (rndr != null && linkedPortal.teleport) rndr.material = newMaterial;
     }
 
     // Update is called once per frame
@@ -61,14 +59,14 @@ public class Portal : MonoBehaviour
     void RotateCamera()
     {
         if (linkedPortal == null || linkedPortal.cam == null) return;
-        Quaternion direction = Quaternion.Inverse(transform.rotation) * inputCam.transform.rotation;
+        Quaternion direction = Quaternion.Inverse(transform.rotation) * Camera.main.transform.rotation;
         linkedPortal.cam.transform.localEulerAngles = direction.eulerAngles + 180 * Vector3.up;
     }
 
     void TranslateCamera()
     {
         if (linkedPortal == null || cam == null) return;
-        Vector3 offset = transform.InverseTransformPoint(inputCam.transform.position);
+        Vector3 offset = transform.InverseTransformPoint(Camera.main.transform.position);
         linkedPortal.cam.transform.localPosition = new Vector3(-offset.x, offset.y, -offset.z);
     }
 
@@ -80,50 +78,81 @@ public class Portal : MonoBehaviour
         Vector4 clipPlaneWorldSpace = new Vector4(planeNormal.x, planeNormal.y, planeNormal.z, distance);
         Matrix4x4 cameraToWorld = cam.worldToCameraMatrix.inverse;
         Vector4 clipPlaneCameraSpace = Matrix4x4.Transpose(cameraToWorld) * clipPlaneWorldSpace;
-        Matrix4x4 newProjectionMatrix = inputCam.CalculateObliqueMatrix(clipPlaneCameraSpace);
+        Matrix4x4 newProjectionMatrix = Camera.main.CalculateObliqueMatrix(clipPlaneCameraSpace);
         cam.projectionMatrix = newProjectionMatrix;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!teleport || linkedPortal == null) return;
-        Rigidbody rb = other.GetComponent<Rigidbody>();
+        if (!teleport || linkedPortal == null || !linkedPortal.teleport || copies.ContainsKey(other) || copies.ContainsValue(other.gameObject) || other.gameObject.name.Contains("Copy")) return;
+        Rigidbody rb = other.attachedRigidbody;
         if (rb == null) return;
-        Vector3 fromPortal = transform.InverseTransformPoint(rb.transform.position);
-        bool inThis = teleportObjects.ContainsKey(other);
-        bool inLinked = linkedPortal.teleportObjects.ContainsKey(other);
-        Character character = other.GetComponent<Character>();
-        bool isCharacter = character != null;
-        if (!inThis && !inLinked && fromPortal.z <= 0.02f)
+        Vector3 offset = transform.InverseTransformPoint(other.transform.position);
+        GameObject copy = Instantiate(other.gameObject, linkedPortal.transform.TransformPoint(new Vector3(-offset.x, offset.y, -offset.z)), linkedPortal.transform.rotation * Quaternion.Inverse(transform.rotation) * other.transform.rotation);
+        copy.name = other.gameObject.name + " Copy";
+        copy.GetComponent<Collider>().enabled = false;
+        foreach (AudioListener listener in copy.GetComponentsInChildren<AudioListener>())
         {
-            int originalLayer = other.gameObject.layer;
-            Vector3 localPos = transform.InverseTransformPoint(rb.position);
-            Quaternion localRot = Quaternion.Inverse(transform.rotation) * rb.rotation;
-            Vector3 localVel = transform.InverseTransformDirection(rb.linearVelocity);
-            Vector3 localAngVel = transform.InverseTransformDirection(rb.angularVelocity);
-            Vector3 newPos = linkedPortal.transform.TransformPoint(new Vector3(-localPos.x, localPos.y, -localPos.z));
-            Quaternion newRot = linkedPortal.transform.rotation * Quaternion.Euler(0, 180, 0) * localRot;
-            Vector3 newVel = linkedPortal.transform.TransformDirection(new Vector3(-localVel.x, localVel.y, -localVel.z));
-            Vector3 newAngVel = linkedPortal.transform.TransformDirection(new Vector3(-localAngVel.x, localAngVel.y, -localAngVel.z));
-            rb.position = newPos;
-            rb.rotation = isCharacter ? Quaternion.Euler(newRot.eulerAngles.y * Vector3.up) : newRot;
-            rb.linearVelocity = newVel;
-            rb.angularVelocity = newAngVel;
-            teleportObjects[other] = originalLayer;
-            linkedPortal.teleportObjects[other] = originalLayer;
+            listener.enabled = false;
         }
-        else if (!inThis)
-        {
-            teleportObjects[other] = other.gameObject.layer;
-        }
+        copies.Add(other, copy);
+        StartCoroutine(MoveCopy(other));
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (!teleport) return;
-        if (teleportObjects.ContainsKey(other))
+        if (!teleport || !copies.ContainsKey(other) || linkedPortal == null || !linkedPortal.teleport || copies.ContainsValue(other.gameObject)) return;
+        bool moveToLinkedPortal = Vector3.Dot(transform.forward, other.transform.position - transform.position) > 0;
+        if (moveToLinkedPortal)
         {
-            teleportObjects.Remove(other);
+            Vector3 offset = transform.InverseTransformPoint(other.transform.position);
+            other.transform.position = linkedPortal.transform.TransformPoint(new Vector3(-offset.x, offset.y, -offset.z));
+            other.transform.rotation = linkedPortal.transform.rotation * Quaternion.Inverse(transform.rotation) * other.transform.rotation * Quaternion.Euler(0, 180, 0);
         }
+        GameObject copy = copies[other];
+        copies.Remove(other);
+        Destroy(copy.gameObject);
+        foreach (Camera c in other.GetComponentsInChildren<Camera>())
+        {
+            c.enabled = true;
+        }
+    }
+
+    IEnumerator MoveCopy(Collider other)
+    {
+        Camera[] cameras = other.GetComponentsInChildren<Camera>();
+        Camera[] camerasC = copies[other].GetComponentsInChildren<Camera>();
+        Dictionary<Camera, Camera> copyCameras = new Dictionary<Camera, Camera>();
+        Dictionary<Transform, Camera> transforms = new Dictionary<Transform, Camera>();
+        int count = 0;
+        foreach (Camera c in cameras)
+        {
+            transforms.Add(c.transform, c);
+            copyCameras.Add(c, camerasC[count]);
+            count++;
+        }
+        while (copies.ContainsKey(other) && copies[other] != null)
+        {
+            foreach (Transform t in transforms.Keys)
+            {
+                bool lookingAtPortal = Vector3.Dot(transform.forward, t.forward) < 0;
+                transforms[t].enabled = lookingAtPortal;
+                copyCameras[transforms[t]].enabled = !lookingAtPortal;
+            }
+            Vector3 offset = transform.InverseTransformPoint(other.transform.position);
+            copies[other].transform.position = linkedPortal.transform.TransformPoint(new Vector3(-offset.x, offset.y, -offset.z));
+            copies[other].transform.rotation = linkedPortal.transform.rotation * Quaternion.Inverse(transform.rotation) * other.transform.rotation * Quaternion.Euler(0, 180, 0);
+            yield return null;
+        }
+    }
+
+    /// <summary>
+    /// Callback to draw gizmos that are pickable and always drawn.
+    /// </summary>
+    void OnDrawGizmos()
+    {
+        if (!teleport) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, cam.transform.position);
     }
 }
