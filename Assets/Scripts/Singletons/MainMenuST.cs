160f474abc8558e3a156f3d882089370271936ab
LUCHER4321 <lucher4321@gmail.com>
2026-03-01 21:52:15 -0300

    Create MainMenuST.cs

---
+++ Assets/Scripts/Singletons/MainMenuST.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuST : MonoBehaviour
+{
+    public static MainMenuST Instance { get; private set; }
+    [SerializeField]
+    GameObject mainMenu;
+    [SerializeField]
+    GameObject optionsMenu;
+    [SerializeField]
+    string gameSceneName;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Options(bool open)
+    {
+        mainMenu.SetActive(!open);
+        optionsMenu.SetActive(open);
+    }
+
+    public void CloseGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+}
