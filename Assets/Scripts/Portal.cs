3f8009eef62c5c63c6c499e19f3adf057b795add
LUCHER4321 <lucher4321@gmail.com>
2026-03-10 22:20:10 -0300

    Update Portal.cs

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -88,66 +88,35 @@ public class Portal : MonoBehaviour
         GameObject copy = copies[other];
         copies.Remove(other);
         Destroy(copy.gameObject);
-        foreach (Camera c in other.GetComponentsInChildren<Camera>())
+        Vector3 offset = transform.InverseTransformPoint(other.transform.position);
+        Vector3 targetPosition = linkedPortal.transform.TransformPoint(new Vector3(-offset.x, offset.y, -offset.z));
+        Quaternion targetRotation = linkedPortal.transform.rotation * Quaternion.Inverse(transform.rotation) * other.transform.rotation * Quaternion.Euler(0, 180, 0);
+        bool portalSide = Vector3.Dot(other.transform.position - transform.position, transform.forward) < 0;
+        if (portalSide)
         {
-            c.enabled = true;
+            other.transform.position = targetPosition;
+            other.transform.rotation = targetRotation;
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                Quaternion relativeRot = linkedPortal.transform.rotation * Quaternion.Inverse(transform.rotation) * Quaternion.Euler(0, 180, 0);
+                rb.linearVelocity = relativeRot * rb.linearVelocity;
+                rb.angularVelocity = relativeRot * rb.angularVelocity;
+            }
         }
     }
 
     IEnumerator MoveCopy(Collider other)
     {
-        Camera[] cameras = other.GetComponentsInChildren<Camera>();
-        Camera[] camerasC = copies[other].GetComponentsInChildren<Camera>();
-        Dictionary<Camera, Camera> copyCameras = new Dictionary<Camera, Camera>();
-        Dictionary<Transform, Camera> transforms = new Dictionary<Transform, Camera>();
-        int count = 0;
-        foreach (Camera c in cameras)
-        {
-            transforms.Add(c.transform, c);
-            copyCameras.Add(c, camerasC[count]);
-            count++;
-        }
-        int portalSideOld = Math.Sign(Vector3.Dot(other.transform.position - transform.position, transform.forward));
         while (copies.ContainsKey(other) && copies[other] != null)
         {
-            foreach (Transform t in transforms.Keys)
-            {
-                Vector3 dirToPortal = transform.position - t.position;
-                bool inFrontOfCamera = Vector3.Dot(t.forward, dirToPortal) > 0;
-                bool onFrontSideOfPortal = Vector3.Dot(transform.forward, -dirToPortal) > 0;
-                bool lookingAtPortal = inFrontOfCamera && onFrontSideOfPortal;
-                transforms[t].enabled = lookingAtPortal;
-                copyCameras[transforms[t]].enabled = !lookingAtPortal;
-            }
             Vector3 offset = transform.InverseTransformPoint(other.transform.position);
             Vector3 targetPosition = linkedPortal.transform.TransformPoint(new Vector3(-offset.x, offset.y, -offset.z));
             Quaternion targetRotation = linkedPortal.transform.rotation * Quaternion.Inverse(transform.rotation) * other.transform.rotation * Quaternion.Euler(0, 180, 0);
             copies[other].transform.position = targetPosition;
             copies[other].transform.rotation = targetRotation;
-            int portalSide = Math.Sign(Vector3.Dot(other.transform.position - transform.position, transform.forward));
-            if (portalSide != portalSideOld)
-            {
-                other.transform.position = targetPosition;
-                other.transform.rotation = targetRotation;
-                Rigidbody rb = other.GetComponent<Rigidbody>();
-                if (rb != null)
-                {
-                    Quaternion relativeRot = linkedPortal.transform.rotation * Quaternion.Inverse(transform.rotation) * Quaternion.Euler(0, 180, 0);
-                    rb.linearVelocity = relativeRot * rb.linearVelocity;
-                    rb.angularVelocity = relativeRot * rb.angularVelocity;
-                }
-                portalSideOld = portalSide;
-            }
-
             yield return null;
         }
-    }
-    void SetObliqueNearClipPlane(Camera playerCam, Camera portalCam, Transform destinationPortal)
-    {
-        Plane portalPlane = new Plane(destinationPortal.forward, destinationPortal.position);
-        Vector4 clipPlaneWorldSpace = new Vector4(portalPlane.normal.x, portalPlane.normal.y, portalPlane.normal.z, portalPlane.distance);
-        Vector4 clipPlaneCameraSpace = Matrix4x4.Transpose(Matrix4x4.Inverse(portalCam.worldToCameraMatrix)) * clipPlaneWorldSpace;
-        portalCam.projectionMatrix = playerCam.CalculateObliqueMatrix(clipPlaneCameraSpace);
     }
 
     /// <summary>
