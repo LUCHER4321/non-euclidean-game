d4f8e445f295190e59d04eacfa6032af3b33a8b5
LUCHER4321 <lucher4321@gmail.com>
2026-04-09 15:56:11 -0400

    Scriptable Objects Update

--- Assets/Scripts/FiniteStateMachine.cs
+++ Assets/Scripts/FiniteStateMachine.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 using System.Linq;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class FiniteStateMachine : MonoBehaviour
 {
     [SerializeField]
@@ -13,6 +17,42 @@ public class FiniteStateMachine : MonoBehaviour
     {
         if (stateMachine == null) return;
         if (stateMachine.GetTransitions(state).Select(x => x.GetEndState).Contains(newState)) state = newState;
+    }
+
+    public void SetState(string newStateName)
+    {
+        if (stateMachine == null) return;
+        if (newStateName == "")
+        {
+            SetState(newState: null);
+            return;
+        }
+#if UNITY_EDITOR
+        {
+            SetState(GetStateAssetsEditorOnly().FirstOrDefault(x => x.name == newStateName));
+            return;
+        }
+#endif
+        SetState(GetStateAssets().FirstOrDefault(x => x.name == newStateName));
+    }
+
+#if UNITY_EDITOR
+    static State[] GetStateAssetsEditorOnly()
+    {
+        string[] guids = AssetDatabase.FindAssets("t:State");
+        State[] states = new State[guids.Length];
+        for (int i = 0; i < guids.Length; i++)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+            states[i] = AssetDatabase.LoadAssetAtPath<State>(path);
+        }
+        return states;
+    }
+#endif
+
+    static State[] GetStateAssets()
+    {
+        return Resources.LoadAll<State>("");
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
