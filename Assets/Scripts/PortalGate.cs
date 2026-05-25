3046a0ffec5c55bbdeee7a83113b734c765ad403
LUCHER4321 <lucher4321@gmail.com>
2026-05-25 00:53:33 -0400

    Update PortalGate.cs

--- Assets/Scripts/PortalGate.cs
+++ Assets/Scripts/PortalGate.cs
@@ -11,9 +11,14 @@ public class PortalGate : Node
 {
     public PortalGateType type;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected override void Start()
     {
         base.Start();
+    }
+
+    public void PStart()
+    {
+        Start();
     }
 
     // Update is called once per frame
