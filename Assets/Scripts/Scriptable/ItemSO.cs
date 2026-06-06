064d0a6fe51b029553312523dab9e83aa05a0428
LUCHER4321 <lucher4321@gmail.com>
2026-06-05 22:56:13 -0400

    Create ItemSO.cs

---
+++ Assets/Scripts/Scriptable/ItemSO.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct ItemShape
+{
+    public Vector2Int[] cells;
+}
+
+[CreateAssetMenu(fileName = "ItemSO", menuName = "Scriptable Objects/ItemSO")]
+public class ItemSO : ScriptableObject
+{
+    [SerializeField]
+    LanText itemName;
+    [SerializeField]
+    int maxStack = 1;
+    [SerializeField]
+    ItemShape[] foldingConfigurations;
+    public string GetItemName { get => itemName.GetText(); }
+    public int GetMaxStack { get => maxStack; }
+    public ItemShape[] GetFoldingConfigurations { get => foldingConfigurations; }
+}
