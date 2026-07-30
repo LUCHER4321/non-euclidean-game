6022f243149675be207cff166315b9214d6f2bb1
LUCHER4321 <lucher4321@gmail.com>
2026-07-30 17:25:54 -0400

    Update PaintPistol.cs

--- Assets/Scripts/Items/PaintPistol.cs
+++ Assets/Scripts/Items/PaintPistol.cs
@@ -2,15 +2,59 @@ using UnityEngine;
 
 public class PaintPistol : Item
 {
-    [SerializeField] private int paintAmount = 100;
-    [SerializeField] private int maxPaintAmount = 100;
-    [SerializeField] private Color paintColor = Color.red;
-    [SerializeField] private Material hopper;
-    private int hopperPropertyID;
+    [SerializeField] int maxPaintAmount = 100;
+    [SerializeField] PaintColor paintColor = new PaintColor(Color.red, 100);
+    [SerializeField] MeshRenderer hopper;
+    private Material hopperMaterial;
+    private int hopperPropertyID, hopperColorPropertyID;
+
+    [System.Serializable]
+    public struct PaintColor
+    {
+        public Color color;
+        public int quantity;
+
+        public PaintColor(Color color, int quantity)
+        {
+            this.color = color;
+            this.quantity = quantity;
+        }
+
+        public PaintColor(params PaintColor[] paints)
+        {
+            if (paints.Length == 0)
+            {
+                color = Color.white;
+                quantity = 0;
+                return;
+            }
+            int totalQuantity = 0;
+            Vector3 cmy = Vector3.zero;
+            float totalAlpha = 0f;
+            foreach (PaintColor paint in paints)
+            {
+                totalQuantity += paint.quantity;
+                Vector3 ncmy = Vector3.one - new Vector3(paint.color.r, paint.color.g, paint.color.b);
+                cmy += ncmy * paint.quantity;
+                totalAlpha += paint.color.a * paint.quantity;
+            }
+            if (totalQuantity == 0)
+            {
+                color = Color.white;
+                quantity = 0;
+                return;
+            }
+            cmy /= totalQuantity;
+            totalAlpha /= totalQuantity;
+            Vector3 rgb = Vector3.one - cmy;
+            color = new Color(rgb.x, rgb.y, rgb.z, totalAlpha);
+            quantity = totalQuantity;
+        }
+    }
 
     public override bool CanUse()
     {
-        return paintAmount > 0;
+        return paintColor.quantity > 0;
     }
 
     public override void Action(bool pressing) { }
@@ -19,16 +63,28 @@ public class PaintPistol : Item
 
     public override bool HandleReload(Item item)
     {
-        if (paintAmount >= maxPaintAmount) return false;
-        paintAmount += Mathf.RoundToInt(item.reloadQuantity);
+        if (paintColor.quantity >= maxPaintAmount || !(item is PaintBottle)) return false;
+        PaintBottle pb = (PaintBottle)item;
+        bool b = true;
+        int q = paintColor.quantity + Mathf.FloorToInt(item.reloadQuantity);
+        if (q > maxPaintAmount)
+        {
+            item.reloadQuantity = q - maxPaintAmount;
+            q = maxPaintAmount;
+            b = false;
+        }
+        paintColor = new PaintColor(paintColor, new PaintColor(pb.paintColor, q - paintColor.quantity));
         UpdateMaterial();
-        return true;
+        return b;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         hopperPropertyID = Shader.PropertyToID("_Paint");
+        hopperColorPropertyID = Shader.PropertyToID("_Paint_Color");
+        if (hopper != null) hopperMaterial = hopper.material;
+        UpdateMaterial();
     }
 
     // Update is called once per frame
@@ -39,6 +95,7 @@ public class PaintPistol : Item
 
     void UpdateMaterial()
     {
-        hopper.SetFloat(hopperPropertyID, Mathf.Clamp((float)paintAmount / (float)maxPaintAmount, 0, 1));
+        hopperMaterial.SetFloat(hopperPropertyID, Mathf.Clamp((float)paintColor.quantity / (float)maxPaintAmount, 0, 1));
+        hopperMaterial.SetColor(hopperColorPropertyID, paintColor.color);
     }
 }
