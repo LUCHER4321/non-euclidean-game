bd87ddccc5127a3e880c4a3a8466e5dcd9022ac3
LUCHER4321 <lucher4321@gmail.com>
2026-07-31 17:30:28 -0400

    Create ItemST.cs

---
+++ Assets/Scripts/Singletons/ItemST.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class ItemST : MonoBehaviour
+{
+    public static ItemST Instance { get; private set; }
+    [SerializeField] Material paintDecal;
+    [SerializeField] GameObject paintDecalPrefab;
+
+    public DecalProjector PaintStain(Color color, Vector3 position, Quaternion rotation)
+    {
+        GameObject spawnedProjector = Instantiate(paintDecalPrefab, position, rotation);
+        DecalProjector spawnedDecalProjector = spawnedProjector.GetComponent<DecalProjector>();
+        if (spawnedDecalProjector == null) return null;
+        Material stainMaterial = new Material(paintDecal);
+        stainMaterial.SetColor("_Color", color);
+        spawnedDecalProjector.material = stainMaterial;
+        return spawnedDecalProjector;
+    }
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this) Destroy(this);
+        else Instance = this;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
