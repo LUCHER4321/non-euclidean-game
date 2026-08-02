636927b9ca3c21504e3c6c2cc739827f3e6dab1f
LUCHER4321 <lucher4321@gmail.com>
2026-08-02 19:26:29 -0400

    Update PaintBottle.cs

--- Assets/Scripts/Items/PaintBottle.cs
+++ Assets/Scripts/Items/PaintBottle.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class PaintBottle : GenericItem
 {
     public Color paintColor = Color.red;
+    [SerializeField] MeshRenderer liquid;
+    float maxPaintAmount { get => (float)PaintPistol.maxPAmount * transform.lossyScale.x * transform.lossyScale.z / (PaintPistol.HopperScale.x * PaintPistol.HopperScale.y * PaintPistol.HopperScale.z) * (3 * transform.lossyScale.y - Mathf.Sqrt(transform.lossyScale.y * transform.lossyScale.z) / 2); }
+    private int liquidPropertyID, liquidColorPropertyID;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        liquidPropertyID = Shader.PropertyToID("_Paint");
+        liquidColorPropertyID = Shader.PropertyToID("_Paint_Color");
+        UpdatePaint();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void UpdatePaint()
+    {
+        if (liquid == null) return;
+        liquid.material.SetFloat(liquidPropertyID, Mathf.Clamp(reloadQuantity / maxPaintAmount, 0, 1));
+        liquid.material.SetColor(liquidColorPropertyID, paintColor);
     }
 }
