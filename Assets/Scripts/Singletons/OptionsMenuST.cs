450763e8bd5f548317c67844c52d59ca2551d2bf
LUCHER4321 <lucher4321@gmail.com>
2026-08-10 23:46:30 -0400

    Update OptionsMenuST.cs

--- Assets/Scripts/Singletons/OptionsMenuST.cs
+++ Assets/Scripts/Singletons/OptionsMenuST.cs
@@ -50,6 +50,15 @@ public class OptionsMenuST : MonoBehaviour
     public bool[] GetInverts { get => inverts; }
     public Language GetLanguage { get => language; }
 
+    private const string PREF_RESOLUTION = "ResIndex",
+    PREF_FULLSCREEN = "Fullscreen",
+    PREF_SENSITIVITY = "Sensitivity",
+    PREF_INVERT_X = "InvertX",
+    PREF_INVERT_Y = "InvertY",
+    PREF_LANGUAGE = "Language",
+    PREF_VOLUME = "Volume_";
+    private const string[] PREF_INVERT = new string[] { PREF_INVERT_X, PREF_INVERT_Y };
+
     static float PercentToDB(float p)
     {
         return 20 * Mathf.Log10(p / 100f);
@@ -83,12 +92,35 @@ public class OptionsMenuST : MonoBehaviour
 
     }
 
+    private void LoadSettings()
+    {
+        resSlider.value = PlayerPrefs.HasKey(PREF_RESOLUTION) ? PlayerPrefs.GetInt(PREF_RESOLUTION) : System.Array.FindIndex(resolutions, x => x.width == Screen.currentResolution.width && x.height == Screen.currentResolution.height);
+        SetResolution();
+        int defaultFullscreen = Screen.fullScreen ? 1 : 0;
+        fullscreenToggle.isOn = PlayerPrefs.GetInt(PREF_FULLSCREEN, defaultFullscreen) == 1;
+        SetFullscreen();
+        sensitivity = PlayerPrefs.GetInt(PREF_SENSITIVITY, defaultSensitivity);
+        senSlider.value = sensitivity;
+        SetSensitivity();
+        for (int i = 0; i < invertTogs.Length; i++)
+        {
+            inverts[i] = PlayerPrefs.GetInt(PREF_INVERT[i], 0) == 1;
+            invertTogs[i].isOn = inverts[i];
+        }
+        string savedLanguage = PlayerPrefs.GetString(PREF_LANGUAGE, language.GetName);
+        int langIndex = System.Array.FindIndex(lanDropdown.options.ToArray(), x => x.text == savedLanguage);
+        lanDropdown.value = langIndex >= 0 ? langIndex : 0;
+        ChangeLanguage();
+    }
+
     public void SetResolution()
     {
         if (resSlider.value < 0 || resSlider.value >= resolutions.Length) return;
         Resolution res = resolutions[(int)resSlider.value];
         Screen.SetResolution(res.width, res.height, Screen.fullScreen);
         resText.text = $"{res.width}x{res.height}";
+        PlayerPrefs.SetInt(PREF_RESOLUTION, (int)resSlider.value);
+        PlayerPrefs.Save();
     }
 
     public void SetFullscreen()
@@ -96,6 +128,8 @@ public class OptionsMenuST : MonoBehaviour
         bool isFullscreen = fullscreenToggle.isOn;
         Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, isFullscreen);
         fullscreenText.text = (isFullscreen ? fullscreenOnText : fullscreenOffText).GetText();
+        PlayerPrefs.SetInt(PREF_FULLSCREEN, isFullscreen ? 1 : 0);
+        PlayerPrefs.Save();
     }
 
     public void SetVolume(VolumeSlider volumeSlider)
@@ -103,6 +137,8 @@ public class OptionsMenuST : MonoBehaviour
         if (volumeSlider == null) return;
         float volume = PercentToDB(volumeSlider.slider.value);
         audioMixer.SetFloat(volumeSlider.outputName, volume);
+        PlayerPrefs.SetFloat(PREF_VOLUME + volumeSlider.outputName, volumeSlider.slider.value);
+        PlayerPrefs.Save();
     }
 
     public void Controls(bool open)
@@ -120,6 +156,8 @@ public class OptionsMenuST : MonoBehaviour
     {
         sensitivity = (int)senSlider.value;
         senText.text = $"{sensitivity}%";
+        PlayerPrefs.SetInt(PREF_SENSITIVITY, sensitivity);
+        PlayerPrefs.Save();
     }
 
     public void SetDefaultSensitivity()
@@ -131,6 +169,8 @@ public class OptionsMenuST : MonoBehaviour
     public void SetInvert(int n)
     {
         inverts[n] = invertTogs[n].isOn;
+        PlayerPrefs.SetInt(PREF_INVERT[n], inverts[n] ? 1 : 0);
+        PlayerPrefs.Save();
     }
 
     public void ChangeLanguage()
@@ -138,5 +178,7 @@ public class OptionsMenuST : MonoBehaviour
         language = languages.First(x => x.GetName == lanDropdown.options[lanDropdown.value].text);
         foreach (MultilanguageText mlt in FindObjectsByType<MultilanguageText>(FindObjectsInactive.Include, FindObjectsSortMode.None)) mlt.ChangeLanguage();
         SetFullscreen();
+        PlayerPrefs.SetString(PREF_LANGUAGE, language.GetName);
+        PlayerPrefs.Save();
     }
 }
