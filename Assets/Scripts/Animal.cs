7bb3d9384de41f0174d47722c3470342c0ec2eb1
LUCHER4321 <lucher4321@gmail.com>
2026-08-25 20:15:55 -0400

    Create Animal.cs

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Animal : Character, IFiniteStateMachine
+{
+    AnimalSO animalSO { get => characterSO as AnimalSO; }
+    [SerializeField] State state;
+    [SerializeField] StateMachine stateMachine;
+    public State CurrentState
+    {
+        get => state;
+        set => state = value;
+    }
+    public StateMachine Machine => stateMachine;
+    private bool hasSons = !animalSO.GetReproduction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnValidate()
+    {
+        if (characterSO != null && !(characterSO is AnimalSO))
+        {
+            Debug.LogWarning("Warning! The Animal script only accepts a CharacterSO characterSO of the AnimalSO sub-class.");
+            characterSO = null;
+        }
+    }
+}
