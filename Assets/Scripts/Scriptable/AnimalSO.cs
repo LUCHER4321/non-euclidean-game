0fa3d15a265227c0a480fbfddbdc1d6d4ef8de0a
LUCHER4321 <lucher4321@gmail.com>
2026-08-25 20:16:23 -0400

    Update AnimalSO.cs

--- Assets/Scripts/Scriptable/AnimalSO.cs
+++ Assets/Scripts/Scriptable/AnimalSO.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
 
+public interface IDistribution
+{
+    float Expectancy();
+    float Variance();
+
+    public float StandardDeviation()
+    {
+        return Mathf.Sqrt(Variance());
+    }
+}
+
 [System.Serializable]
-public struct FloatDistribution
+public struct FloatDistribution : IDistribution
 {
     public enum DistType
     {
@@ -45,10 +56,42 @@ public struct FloatDistribution
             default: return 0f;
         }
     }
+
+    public float Expectancy()
+    {
+        switch (distType)
+        {
+            case DistType.Uniform: return (min + max) / 2f;
+            case DistType.Exponential: return 1f / lambda;
+            case DistType.Triangular: return (min + mode + max) / 3f;
+            case DistType.Erlang: return m * beta;
+            case DistType.Normal: return mu;
+            case DistType.LogNormal: return mu;
+            case DistType.Gamma: return shape * scale;
+            case DistType.Weibull: return scale * Mathf.Gamma(1f + 1f / shape);
+            default: return 0f;
+        }
+    }
+
+    private float Variance()
+    {
+        switch (distType)
+        {
+            case DistType.Uniform: return Mathf.Pow(max - min, 2f) / 12f;
+            case DistType.Exponential: return 1f / (lambda * lambda);
+            case DistType.Triangular: return (min * min + max * max + mode * mode - min * max - min * mode - max * mode) / 18f;
+            case DistType.Erlang: return m * beta * beta;
+            case DistType.Normal: return sigma * sigma;
+            case DistType.LogNormal: return sigma * sigma;
+            case DistType.Gamma: return shape * scale * scale;
+            case DistType.Weibull: return scale * scale * (Mathf.Gamma(1f + 2f / shape) - Mathf.Pow(Mathf.Gamma(1f + 1f / shape), 2f));
+            default: return 0f;
+        }
+    }
 }
 
 [System.Serializable]
-public struct IntDistribution
+public struct IntDistribution : IDistribution
 {
     public enum DistType
     {
@@ -76,6 +119,28 @@ public struct IntDistribution
             case DistType.Binomial: return RandomDistribution.Binomial(n, p);
             case DistType.NegBinomial: return RandomDistribution.NegBinomial(r, p);
             default: return 0;
+        }
+    }
+
+    public float Expectancy()
+    {
+        switch (distType)
+        {
+            case DistType.Poisson: return lambda;
+            case DistType.Binomial: return n * p;
+            case DistType.NegBinomial: return r * (1f - p) / p;
+            default: return 0f;
+        }
+    }
+
+    private float Variance()
+    {
+        switch (distType)
+        {
+            case DistType.Poisson: return lambda;
+            case DistType.Binomial: return n * p * (1f - p);
+            case DistType.NegBinomial: return r * (1f - p) / (p * p);
+            default: return 0f;
         }
     }
 }
@@ -116,14 +181,60 @@ public class AnimalSO : CharacterSO
             RandomDistribution.Normal(meanScale.z, stdDevScale.z)
         );
     }
+    public Vector3 GetExpectedScale { get => meanScale; }
+    public Vector3 GetStdDevScale { get => stdDevScale; }
     public float GetMass { get => mass.Evaluate(); }
+    public float GetExpectedMass { get => mass.Expectancy(); }
+    public float GetStandardDeviationMass { get => mass.StandardDeviation(); }
     public bool GetReproduction { get => RandomDistribution.Bernoulli(reproductionChance); }
+    public int GetSons { get => sons.Evaluate(); }
+    public float GetExpectedSons { get => sons.Expectancy(); }
+    public float GetStandardDeviationSons { get => sons.StandardDeviation(); }
     public float GetLifeExpectancy { get => lifeExpectancy.Evaluate(); }
-    public float[] GetChildbearingAge
+    public float GetExpectedLifeExpectancy { get => lifeExpectancy.Expectancy(); }
+    public float GetStandardDeviationLifeExpectancy { get => lifeExpectancy.StandardDeviation(); }
+    public Vector2 GetChildbearingAge
     {
-        get => new float[] {
+        get => new Vector2(
             childbearingAge.min.Evaluate(),
             childbearingAge.max.Evaluate()
-        };
+        );
+    }
+    public Vector2 GetExpectedChildbearingAge
+    {
+        get => new Vector2(
+            childbearingAge.min.Expectancy(),
+            childbearingAge.max.Expectancy()
+        );
+    }
+    public Vector2 GetStandardDeviationChildbearingAge
+    {
+        get => new Vector2(
+            childbearingAge.min.StandardDeviation(),
+            childbearingAge.max.StandardDeviation()
+        );
+    }
+
+    public static AnimalSO GetRandomSpecies(AnimalSO[] species)
+    {
+        if (species == null || species.Length == 0) return null;
+        float totalBiomass = 0f;
+        float[] biomasses = new float[species.Length];
+        for (int i = 0; i < species.Length; i++)
+        {
+            AnimalSO sp = species[i];
+            float biomass = 0;
+            for (int j = 0; j < sp.GetSons; j++) biomass += sp.GetLifeExpectancy * sp.GetMass;
+            biomasses[i] = biomass;
+            totalBiomass += biomass;
+        }
+        float randomValue = RandomDistribution.Uniform(0f, totalBiomass);
+        float cumulativeBiomass = 0f;
+        for (int i = 0; i < species.Length; i++)
+        {
+            cumulativeBiomass += biomasses[i];
+            if (cumulativeBiomass >= randomValue) return species[i];
+        }
+        return null;
     }
 }
