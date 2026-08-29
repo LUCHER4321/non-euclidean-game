313fc2bd3ddd860683d214ca79ce95e32ff1a11c
LUCHER4321 <lucher4321@gmail.com>
2026-08-28 21:09:42 -0400

    Update PortalGate.cs

--- Assets/Scripts/PortalGate.cs
+++ Assets/Scripts/PortalGate.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Linq;
 
 public enum PortalGateType
 {
@@ -10,10 +11,12 @@ public enum PortalGateType
 public class PortalGate : Node
 {
     public PortalGateType type;
+    public Portal portal { get; private set; }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     protected override void Start()
     {
         base.Start();
+        portal = GetComponentsInChildren<Portal>().FirstOrDefault(x => x.GetTeleport);
     }
 
     public void PStart()
